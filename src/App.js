cc798a818105c9c4ab9a1daf7525868592afab25
otimthomas <totim444@hotmail.co.uk>
2020-07-03 10:51:54 +0300

    NewsLetter Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.scss';
 import Navbar from './components/Navbar';
 import ShowCase from './components/ShowCase';
+import NewsLetter from './components/NewsLetter';
+import Boxes from './components/Boxes';
 
 function App() {
   return (
     <>
       <Navbar />
       <ShowCase />
+      <NewsLetter />
+      <Boxes />
     </>
   );
 }
